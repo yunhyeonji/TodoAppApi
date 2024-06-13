91ae6e83a8495642364821f3ce351111dd289763
yhj <yhj@YHJ-MacBook-Air.local>
2024-06-13 20:05:45 +0900

    first commit

---
+++ src/main/java/org/yunhj/todoApp/todoAppApplication.java
@@ -0,0 +1,11 @@
+package org.yunhj.todoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class todoAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(todoAppApplication.class, args);
+    }
+}
